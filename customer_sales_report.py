934ab9671c7697346e5429129812165a4bcf51fa
madimcgaughey <madi_mcgaughey1@baylor.edu>
2023-01-30 23:21:25 -0600

    hello

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -1,4 +1,14 @@
 import csv
 
 sales_file = open('sales.csv','r')
+reader = csv.reader(sales_file)
+sales_file.append('Total Cost')
+for item in row:
+    item.append(item[0])
+    print(item)
+
+
 sales_report_file = open('salesreport.csv','w')
+writer = csv.writer(sales_report_file, delimiter=' ')
+for column in reader:
+    writer.writerow((column[0],column[3]))
