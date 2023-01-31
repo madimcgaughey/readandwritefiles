801a3ad73e1d4b69775b64ef3f4e789ea233429d
madimcgaughey <madi_mcgaughey1@baylor.edu>
2023-01-30 22:16:25 -0600

    change

--- customer_country.py
+++ customer_country.py
@@ -1,5 +1,22 @@
 import csv
 
 customers_file = open('customers.csv','r')
-customer_country_file = open('customer_country.csv','w')
+reader = csv.reader(customers_file)
 
+
+customer_country_file = open('customer_country.csv','w')
+writer = csv.writer(customer_country_file, delimiter = ' ')
+for column in reader:
+    writer.writerow((column[1],column[2],column[4]))
+    
+
+
+
+
+
+
+
+
+
+#writer.writerow(column_name)
+#writer.writerow(data)
