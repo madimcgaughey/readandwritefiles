956328fda4fec1e640c5cb138c754f5f63242e17
madimcgaughey <madi_mcgaughey1@baylor.edu>
2023-01-30 22:16:17 -0600

    change

--- employee_bonus.py
+++ employee_bonus.py
@@ -5,11 +5,11 @@ bonus = csv.reader(employee_bonus, delimiter = ',')
 next(bonus)
 for record in bonus:
     print(record)
-    print("Employee ID:", record[0])
-    print("Employee First Name:", record[1])
-    print("Employee Last Name:", record[2])
-    print("Salary:",record[3])
-    print("Bonus:",record[4])
+    print('Employee ID:', record[0])
+    print('Employee First Name:', record[1])
+    print('Employee Last Name:', record[2])
+    print('Salary:',record[3])
+    print('Bonus:',record[4])
     input()  
 
 
