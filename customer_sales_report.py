99a466c3e68dde283089e311648d2a56fe34857a
madimcgaughey <madi_mcgaughey1@baylor.edu>
2023-01-30 23:39:36 -0600

    changes

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -2,10 +2,7 @@ import csv
 
 sales_file = open('sales.csv','r')
 reader = csv.reader(sales_file)
-sales_file.append('Total Cost')
-for item in row:
-    item.append(item[0])
-    print(item)
+
 
 
 sales_report_file = open('salesreport.csv','w')
